fbc5a404945ffbbb1ecd7b666c9cdb76c99bfb3c
tiemies fait <69427473+fa1t666@users.noreply.github.com>
2021-04-12 19:00:26 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,24 +1,10 @@
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(100000)
-	end
-end)
-
-local ekaspawn = true
-player = PlayerId()
-AddEventHandler("playerSpawned", function()
-    if ekaspawn then
-    SetPlayerControl(player, false, 0)
-    ekaspawn = false
-    Wait(7000)
-    TriggerServerEvent("logit:logitx")
-    SetPlayerControl(player, true, 0)
-    ekaspawn = false
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(100000)
     end
 end)
 
-
 exports('discord', function(message, color, channel)
-	TriggerServerEvent('clienttidiscordi', message, color, channel)
+    TriggerServerEvent('clienttidiscordi', message, color, channel)
 end)
