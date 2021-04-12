2cd76579069ef4506033a15cf9d451c8cebc0969
tiemies fait <69427473+fa1t666@users.noreply.github.com>
2021-04-12 19:00:48 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,30 +1,33 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+local alreadylogged = {}
 
-RegisterNetEvent("logit:logitx")
-AddEventHandler("logit:logitx", function()
-local xPlayer = ESX.GetPlayerFromId(source)
-local nimi = xPlayer.getName()
-local ryhma = xPlayer.getGroup()
-local tyo = xPlayer.job.name
-local arvo = xPlayer.job.grade
-local Rahat = xPlayer.getMoney()
-local aseet = xPlayer.getLoadout().name
-local aseetlogi = json.encode(aseet)
-local Pankki = xPlayer.getAccount('bank').money
-local Paskaset = xPlayer.getAccount('black_money').money
-local ip = GetPlayerEndpoint(source)
-local ping = GetPlayerPing(source)
-local ids = ExtractIdentifiers(source)
-if Config.xblID then xblID ="\n**Xbox ID:  ** " ..ids.xbl.."" else xblID = "" end
-if Config.steamID then _steamID ="\n**Steam ID:  ** " ..ids.steam.."" else _steamID = "" end
-if Config.liveID then _liveID ="\n**Live ID:  ** " ..ids.live.."" else _liveID = "" end
-if Config.playerID then _playerID ="\n**Player ID:  ** " ..source.."" else _playerID = "" end
-if Config.discordID then _discordID ="\n**Discord ID:  ** <@" ..ids.discord:gsub("discord:", "")..">" else _discordID = "" end
-if Config.licenseID then _licenseID ="\n**License ID:  ** " ..ids.license.."" else _licenseID = "" end
-if Config.steamURL then _steamURL ="\n\n **Steam Url  **https://steamcommunity.com/profiles/" ..tonumber(ids.steam:gsub("steam:", ""),16).."" else _steamURL = "" end
-renterdiscord('**Pelaaja uhdisti palvelimelle:**' ..nimi.. '\n' .._playerID.. '\n ' .._steamID.. ' ' .._steamURL.. '\n' .._discordID.. '\n'.._licenseID.. '\n' ..xblID.. '\n' .._liveID.. '\n\n**IP:**' ..ip.. '\n\n**Pinggi:** ' ..ping.. '\n\n**Ryhmä:**' ..ryhma..'\n\n**Käteinen: **' ..Rahat..'\n\n**Pankki: **' ..Pankki..'\n\n**Likainen raha: **' ..Paskaset..'\n\n**Työ: **' ..tyo..'\n\n**Työn arvo: **' ..arvo..'\n\n**Aseet: **' ..aseetlogi..'')
+AddEventHandler("esx:playerLoaded", function(source)
+    if not alreadylogged[source] then
+        local xPlayer = ESX.GetPlayerFromId(source)
+        local nimi = xPlayer.getName()
+        local ryhma = xPlayer.getGroup()
+        local tyo = xPlayer.job.label
+        local arvo = xPlayer.job.grade_label
+        local Rahat = xPlayer.getMoney()
+        local aseet = xPlayer.getLoadout().name
+        local aseetlogi = json.encode(aseet)
+        local Pankki = xPlayer.getAccount('bank').money
+        local Paskaset = xPlayer.getAccount('black_money').money
+        local ip = GetPlayerEndpoint(source)
+        local ping = GetPlayerPing(source)
+        local ids = ExtractIdentifiers(source)
+        if Config.xblID then xblID ="\n**Xbox ID:  ** " ..ids.xbl.."" else xblID = "" end
+        if Config.steamID then _steamID ="\n**Steam ID:  ** " ..ids.steam.."" else _steamID = "" end
+        if Config.liveID then _liveID ="\n**Live ID:  ** " ..ids.live.."" else _liveID = "" end
+        if Config.playerID then _playerID ="\n**Player ID:  ** " ..source.."" else _playerID = "" end
+        if Config.discordID then _discordID ="\n**Discord ID:  ** <@" ..ids.discord:gsub("discord:", "")..">" else _discordID = "" end
+        if Config.licenseID then _licenseID ="\n**License ID:  ** " ..ids.license.."" else _licenseID = "" end
+        if Config.steamURL then _steamURL ="\n\n **Steam Url  **https://steamcommunity.com/profiles/" ..tonumber(ids.steam:gsub("steam:", ""),16).."" else _steamURL = "" end
+        renterdiscord('**Pelaaja yhdisti palvelimelle**: ' ..nimi.. '\n' .._playerID.. '\n ' .._steamID.. ' ' .._steamURL.. '\n' .._discordID.. '\n'.._licenseID.. '\n' ..xblID.. '\n' .._liveID.. '\n\n**IP:**' ..ip.. '\n\n**Pinggi:** ' ..ping.. '\n\n**Ryhmä:**' ..ryhma..'\n\n**Käteinen: **' ..Rahat..'\n\n**Pankki: **' ..Pankki..'\n\n**Likainen raha: **' ..Paskaset..'\n\n**Työ: **' ..tyo..'\n\n**Työn arvo: **' ..arvo..'\n\n**Aseet: **' ..aseetlogi..'')
+        alreadylogged[source] = true
+    end
 end) 
 
 function renterdiscord(message)
@@ -38,32 +41,31 @@ function renterdiscord(message)
             }, 
         }
     }
-        
-  	PerformHttpRequest( "WEBHOOK TÄHÄ" , function(err, text, headers) end, 'POST', json.encode({username = name, embeds = content}), { ['Content-Type'] = 'application/json' })
+  	PerformHttpRequest( "https://canary.discord.com/api/webhooks/827612874620796998/L1Vmh9EhTOGOJ8CFOnwB5hISXbQQQUQtVEg-5rvNrjokwnK7g55dQvnpuV7eYC7cEJmI" , function(err, text, headers) end, 'POST', json.encode({username = name, embeds = content}), { ['Content-Type'] = 'application/json' })
 end
 
-AddEventHandler("playerDropped", function()
-local xPlayer = ESX.GetPlayerFromId(source)
-local nimi = xPlayer.getName()
-local ryhma = xPlayer.getGroup()
-local tyo = xPlayer.job.name
-local arvo = xPlayer.job.grade
-local Rahat = xPlayer.getMoney()
-local Pankki = xPlayer.getAccount('bank').money
-local Paskaset = xPlayer.getAccount('black_money').money
-local ip = GetPlayerEndpoint(source)
-local aseet = xPlayer.getLoadout().name
-local aseetlogi = json.encode(aseet)
-local ping = GetPlayerPing(source)
-local ids = ExtractIdentifiers(source)
-if Config.xblID then xblID ="\n**Xbox ID:  ** " ..ids.xbl.."" else xblID = "" end
-if Config.steamID then _steamID ="\n**Steam ID:  ** " ..ids.steam.."" else _steamID = "" end
-if Config.liveID then _liveID ="\n**Live ID:  ** " ..ids.live.."" else _liveID = "" end
-if Config.playerID then _playerID ="\n**Player ID:  ** " ..source.."" else _playerID = "" end
-if Config.discordID then _discordID ="\n**Discord ID:  ** <@" ..ids.discord:gsub("discord:", "")..">" else _discordID = "" end
-if Config.licenseID then _licenseID ="\n**License ID:  ** " ..ids.license.."" else _licenseID = "" end
-if Config.steamURL then _steamURL ="\n\n **Steam Url  **https://steamcommunity.com/profiles/" ..tonumber(ids.steam:gsub("steam:", ""),16).."" else _steamURL = "" end
-rexitdiscord('**Pelaaja painu vittuun servulta:  **' ..nimi.. '\n' .._playerID.. '\n ' .._steamID.. ' ' .._steamURL.. '\n' .._discordID.. '\n'.._licenseID.. '\n' ..xblID.. '\n' .._liveID.. '\n\n**IP:  **' ..ip.. '\n\n**Pinggi:  ** ' ..ping.. '\n\n**Ryhmä:  **' ..ryhma..'\n\n**Käteinen:  **' ..Rahat..'\n\n**Pankkissa rahaa: **' ..Pankki..'\n\n**Likainen raha: **' ..Paskaset..'\n\n**Työ: **' ..tyo..'\n\n**Työn arvo: **' ..arvo..'\n\n**Aseet: **' ..aseetlogi..'')
+AddEventHandler("playerDropped", function(reason)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local nimi = xPlayer.getName()
+    local ryhma = xPlayer.getGroup()
+    local tyo = xPlayer.job.name
+    local arvo = xPlayer.job.grade
+    local Rahat = xPlayer.getMoney()
+    local Pankki = xPlayer.getAccount('bank').money
+    local Paskaset = xPlayer.getAccount('black_money').money
+    local ip = GetPlayerEndpoint(source)
+    local aseet = xPlayer.getLoadout().name
+    local aseetlogi = json.encode(aseet)
+    local ping = GetPlayerPing(source)
+    local ids = ExtractIdentifiers(source)
+    if Config.xblID then xblID ="\n**Xbox ID:  ** " ..ids.xbl.."" else xblID = "" end
+    if Config.steamID then _steamID ="\n**Steam ID:  ** " ..ids.steam.."" else _steamID = "" end
+    if Config.liveID then _liveID ="\n**Live ID:  ** " ..ids.live.."" else _liveID = "" end
+    if Config.playerID then _playerID ="\n**Player ID:  ** " ..source.."" else _playerID = "" end
+    if Config.discordID then _discordID ="\n**Discord ID:  ** <@" ..ids.discord:gsub("discord:", "")..">" else _discordID = "" end
+    if Config.licenseID then _licenseID ="\n**License ID:  ** " ..ids.license.."" else _licenseID = "" end
+    if Config.steamURL then _steamURL ="\n\n **Steam Url  **https://steamcommunity.com/profiles/" ..tonumber(ids.steam:gsub("steam:", ""),16).."" else _steamURL = "" end
+    rexitdiscord('**Pelaaja painu vittuun servulta: **' ..nimi.. '\n\n**Syy:** '..reason..'\n' .._playerID.. '\n ' .._steamID.. ' ' .._steamURL.. '\n' .._discordID.. '\n'.._licenseID.. '\n' ..xblID.. '\n' .._liveID.. '\n\n**IP:  **' ..ip.. '\n\n**Pinggi:  ** ' ..ping.. '\n\n**Ryhmä:  **' ..ryhma..'\n\n**Käteinen:  **' ..Rahat..'\n\n**Pankkissa rahaa: **' ..Pankki..'\n\n**Likainen raha: **' ..Paskaset..'\n\n**Työ: **' ..tyo..'\n\n**Työn arvo: **' ..arvo..'\n\n**Aseet: **' ..aseetlogi..'')
 end) 
 
 function rexitdiscord(message)
@@ -78,20 +80,9 @@ function rexitdiscord(message)
             }, 
         }
     }
-        
-  	PerformHttpRequest( "WEBHOOKKI" , function(err, text, headers) end, 'POST', json.encode({username = name, embeds = content}), { ['Content-Type'] = 'application/json' })
+  	PerformHttpRequest( "https://canary.discord.com/api/webhooks/827612874620796998/L1Vmh9EhTOGOJ8CFOnwB5hISXbQQQUQtVEg-5rvNrjokwnK7g55dQvnpuV7eYC7cEJmI" , function(err, text, headers) end, 'POST', json.encode({username = name, embeds = content}), { ['Content-Type'] = 'application/json' })
 end
-RegisterServerEvent('clienttidiscordi')
-AddEventHandler('clienttidiscordi', function(message, color, channel)
-   discordLog(message, color,  channel)
-end)
 
-
-RegisterServerEvent('logit:identifierit')
-AddEventHandler('logit:identifierit', function(src)
-	local ids = ExtractIdentifiers(src)
-	return ids
-end)
 
 function ExtractIdentifiers(src)
     local identifiers = {
@@ -102,7 +93,6 @@ function ExtractIdentifiers(src)
         xbl = "",
         live = ""
     }
-
     for i = 0, GetNumPlayerIdentifiers(src) - 1 do
         local id = GetPlayerIdentifier(src, i)
 
@@ -120,6 +110,5 @@ function ExtractIdentifiers(src)
             identifiers.live = id
         end
     end
-
     return identifiers
 end
